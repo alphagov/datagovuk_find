fff9ed096105406cdbc56b72cc4cefb2f4cceb1b
Max Froumentin <github@lapin-bl.eu>
2017-07-21 14:53:09 +0100

    Add http password for hiding beta

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Find Data Beta
 
 
+In order for the app to work, you need to set two environment variables:
+`FIND_USERNAME` and `FIND_PASSWORD`. They need to be set in order to be able to
+log in.
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
+  httpauth_name = ENV.fetch('FIND_USERNAME', rand(100000000).to_s)
+  httpauth_pass = ENV.fetch('FIND_PASSWORD', rand(100000000).to_s)
+  http_basic_authenticate_with name: httpauth_name, password: httpauth_pass
   protect_from_forgery with: :exception
 end
