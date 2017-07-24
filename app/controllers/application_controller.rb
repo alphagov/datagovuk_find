f4c70439c44e2ac14212978364049dfe85ec337a
Toby Retallick <toby.retallick@gmail.com>
2017-07-24 14:16:15 +0100

    Create dataset route, controller and view scaffold

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate
+  # before_action :authenticate
   protect_from_forgery with: :exception
 end
 
 
-def authenticate
-  httpauth_name = ENV['FIND_USERNAME']
-  httpauth_pass = ENV['FIND_PASSWORD']
-  authenticate_or_request_with_http_basic('Administration') do |username, password|
-    username == httpauth_name && password == httpauth_pass
-  end
-end
+# def authenticate
+  # httpauth_name = ENV['FIND_USERNAME']
+  # httpauth_pass = ENV['FIND_PASSWORD']
+  # authenticate_or_request_with_http_basic('Administration') do |username, password|
+  #   username == httpauth_name && password == httpauth_pass
+  # end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
-
+  resources :dataset do
+    get 'dataset/1234', to: 'dataset#show'
+  end
 end
