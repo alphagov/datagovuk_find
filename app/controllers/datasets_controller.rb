b58d71cc34eb82d14d2d6b3c439dfa89c63e7699
Toby Retallick <toby.retallick@gmail.com>
2017-07-24 17:05:41 +0100

    Create time series data

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -1,4 +1,23 @@
 class DatasetsController < ApplicationController
   def show
+    @groups = [
+        {
+            year: '2016',
+            datafiles: [
+                {
+                    name: 'Datafile A'
+                }
+            ]
+        },
+        {
+            year: '2018',
+            datafiles: [
+                {
+                    name: 'Datafile B'
+                }
+            ]
+        }
+    ]
+
   end
 end
