d932291e1b3d9fd6ad28ea419a4bf1f82cd36786
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2017-07-24 16:06:41 +0100

    refactor datasets routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
-  resources :datasets do
-    get 'dataset/:id', to: 'datasets#show'
-  end
+  resources :datasets, :path => "dataset", only: :show
 end
--- spec/features/dataset_show_page_spec.rb
+++ spec/features/dataset_show_page_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
   describe "Datasets" do
     it "displays a dataset" do
-      visit "/datasets/1234"
-      expect(page).to have_content("Dataset")
+      visit "/dataset/1234"
+      expect(page).to have_content("Published by")
     end
   end
