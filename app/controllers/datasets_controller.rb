809e5a6e0244ad078180ac409e25f3dace8d6994
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2017-07-26 13:57:58 +0100

    contact partial

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -38,5 +38,9 @@ class DatasetsController < ApplicationController
         last_updated: '25-12-14',
         format: 'CSV'}
   ]
+  @organisation = {
+    contact_name: "contact lady",
+    contact_email:"contact_lady@hello.com",
+    foi_name: 'foi man', foi_email: 'foi_man@hello.com'}
   end
 end
