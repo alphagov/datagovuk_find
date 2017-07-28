0f2c2ddd1e5c4dd0c9002239dcd6be920334ee4c
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2017-07-28 22:11:01 +0100

    wires up the search bar on the dataset show page, and adds the search tips page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'search/', to: 'search#search'
+  get 'search/tips', to: 'search#tips'
   root to: 'home#index'
 
   resources :datasets, :path => "dataset", only: :show
