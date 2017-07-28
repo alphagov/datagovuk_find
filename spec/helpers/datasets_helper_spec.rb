390ffb22fdee658a55c390b4135f41d7ba387a66
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2017-07-28 22:18:02 +0100

    typo

--- spec/helpers/datasets_helper_spec.rb
+++ spec/helpers/datasets_helper_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 # Specs in this file have access to a helper object that includes
-# the DocsHelper. For example:
+# the DatasetsHelper. For example:
 #
-# describe DocsHelper do
+# describe DatasetsHelper do
 #   describe "string concat" do
 #     it "concats two strings with spaces" do
 #       expect(helper.concat_strings("this","that")).to eq("this that")
