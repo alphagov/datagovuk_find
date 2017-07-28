a6bd20b789d32aa559dcbabef82329dcff9db052
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2017-07-28 14:12:12 +0100

    calculates expected update

--- app/helpers/datasets_helper.rb
+++ app/helpers/datasets_helper.rb
@@ -1,5 +1,18 @@
 module DatasetsHelper
 
+  FREQUENCIES = {
+    'annual' => { years: 1 },
+    'quarterly' => { months: 4 },
+    'monthly' => { months: 1 },
+    'daily' => { days: 1 }
+  }
+
+  NO_MORE = {
+    'discontinued' => 'Dataset no longer updated',
+    'never' => 'No future updates',
+    'one off' => 'No future updates'
+  }
+
   def documents
     docs = []
     @dataset['datafiles'].each do |file|
@@ -22,5 +35,19 @@ module DatasetsHelper
   def documentation?(key)
     key != nil && key != ""
   end
+
+  def expected_update
+    last = @dataset["updated_at"]
+    freq = @dataset["frequency"]
+
+    if FREQUENCIES.has_key?(freq)
+      "<dd>#{format(last.advance(FREQUENCIES[freq]))}</dd>".html_safe
+    elsif NO_MORE.has_key?(freq)
+      "<dd class='unavailable'> #{NO_MORE[freq]}</dd>".html_safe
+    else
+      "<dd class='unavailable'> Not available </dd>".html_safe
+    end
+  end
+
 
 end
