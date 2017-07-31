e55f6aff77cb1c8ee93ac5be6c95dedd081af338
Laurence <laurence.smith@digital.cabinet-office.gov.uk>
2017-07-31 17:23:32 +0100

    Add (bad) recency sort

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,17 +1,28 @@
 class SearchController < ApplicationController
   def search
     @query = params["q"]
-    datasets = Dataset.search({ q: @query })
-    @results = datasets.datasets
-    @num_results = datasets.num_results
     @sorted_by = sort
     @location = location
+    results = Dataset.search({ q: @query })
+    @datasets = results.datasets
+    @num_results = results.num_results
+
+    sort_datasets!
   end
 
   private
   def sort
     sort = params["sortby"]
     %w(best recent viewed).include?(sort) ? sort : nil
+  end
+
+  def sort_datasets!
+    return unless @sorted_by
+
+    case @sorted_by
+    when "recent"
+      @dataset = @datasets.sort_by { |d| d.created_at }
+    end
   end
 
   def location
