7667254ce5af19d864bcc501a17b843d43671940
Toby Retallick <toby.retallick@gmail.com>
2017-08-03 10:28:46 +0100

    Add breadcrumb to dataset view page

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -1,6 +1,9 @@
+require 'cgi'
+
 class DatasetsController < ApplicationController
   include DatasetsHelper
   def show
+    @query = get_query_referral
     @dataset = current_dataset
   end
 
@@ -8,5 +11,10 @@ class DatasetsController < ApplicationController
 
   def current_dataset
     Dataset.get({id: params[:id]})._source
+  end
+
+  def get_query_referral
+    query = URI(request.referrer).query
+    CGI::parse(query)['q'].join
   end
 end
