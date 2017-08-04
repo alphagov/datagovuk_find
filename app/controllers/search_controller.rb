661900eb5f42b7aa74e6726a5de806f0a9a0c88f
Laurence Smith <laurence.smith@digital.cabinet-office.gov.uk>
2017-08-04 15:21:13 +0100

    Elastic filtering and sorting (#78)
    * Add back pagination, fix sorting behaviour
    
    * Mostly working filters -- slightly fuzzy match instead of direct match

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,19 +1,51 @@
 class SearchController < ApplicationController
   def search
     @query = params["q"]
-    @search = Dataset.search(@query)
     @sorted_by = sort
     @location = location
     @organisation = organisation
-    # @datasets = @search.page(page_number)
-    @datasets =  process_results(@search.results)
-    @num_results = @datasets.count
+
+    @search = Dataset.search(search_query)
+    @num_results = @search.results.total_count
+    @datasets = @search.page(page_number)
   end
 
   def tips
   end
 
   private
+  def search_query
+    base_search_query = {
+      query: {
+        bool: {
+          must: {
+            multi_match: {
+              query: @query,
+              fields: %w(title summary description organisation^2 location*^2)
+            }
+          }
+        }
+      }
+    }
+
+    case @sorted_by
+    when "recent"
+      base_search_query[:sort] = { updated_at: :desc }
+    end
+
+    if @location
+      base_search_query[:query][:bool][:filter] ||= []
+      base_search_query[:query][:bool][:filter] << { term: { location1: @location } }
+    end
+
+    if @organisation
+      base_search_query[:query][:bool][:filter] ||= []
+      base_search_query[:query][:bool][:filter] << { match: { :"organisation.title" => @organisation } }
+    end
+
+    base_search_query
+  end
+
   def page_number
     page = params["page"]
 
@@ -37,26 +69,5 @@ class SearchController < ApplicationController
   def organisation
     org = params["org"] || params["input-autocomplete"]
     org.blank? ? nil : org
-  end
-
-  def process_results(results)
-    results = process_organisation(results)
-    process_location(results)
-  end
-
-  def process_organisation(results)
-    if @organisation.nil?
-      results
-    else
-      results.select { |r| r.organisation.title == @organisation }
-    end
-  end
-
-  def process_location(results)
-    if @location.nil?
-      results
-    else
-      results.select { |r| r.location1 == @location }
-    end
   end
 end
