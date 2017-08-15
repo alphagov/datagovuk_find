aacf5120ae2a9ee8bd3789b009786216aac8960d
Laurence <laurence.smith@digital.cabinet-office.gov.uk>
2017-08-15 16:52:14 +0100

    Add preview route and preview fetching, link up previews

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -18,6 +18,13 @@ class DatasetsController < ApplicationController
     end
   end
 
+  def preview
+    @preview = RestClient.get(preview_url(params[:file_id]))
+    render json: @preview
+  rescue
+    render json: { "error": "No preview available" }, status: 404
+  end
+
   private
 
   def handle_error(e)
