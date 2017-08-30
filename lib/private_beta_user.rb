f7fcacd7b48bf129208103b815d2395c3babee44
Laurence Smith <laurence.smith@digital.cabinet-office.gov.uk>
2017-08-30 11:03:45 +0100

    Better wordlist (#191)
    * Add EFF wordlist
    
    * Add loader for wordlist
    
    * User better words

--- lib/private_beta_user.rb
+++ lib/private_beta_user.rb
@@ -2,51 +2,17 @@ module PrivateBetaUser
   BLACKLIST = ENV.fetch('PRIVATE_BETA_USER_BLACKLIST', '').split(';')
   SALT = ENV.fetch("PRIVATE_BETA_USER_SALT", '')
 
-  MEMORABLE_WORDS = {
-    'a' => 'alpha',
-    'b' => 'bravo',
-    'c' => 'charlie',
-    'd' => 'delta',
-    'e' => 'echo',
-    'f' => 'foxtrot',
-    'g' => 'golf',
-    'h' => 'hotel',
-    'i' => 'india',
-    'j' => 'juliet',
-    'k' => 'kilo',
-    'l' => 'lima',
-    'm' => 'mike',
-    'n' => 'november',
-    'o' => 'oscar',
-    'p' => 'papa',
-    'q' => 'quebec',
-    'r' => 'romeo',
-    's' => 'sierra',
-    't' => 'tango',
-    'u' => 'uniform',
-    'v' => 'victor',
-    'w' => 'whiskey',
-    'x' => 'xray',
-    'y' => 'yankee',
-    'z' => 'zulu',
-    '0' => 'zero',
-    '1' => 'one',
-    '2' => 'two',
-    '3' => 'three',
-    '4' => 'four',
-    '5' => 'five',
-    '6' => 'six',
-    '7' => 'seven',
-    '8' => 'eight',
-    '9' => 'nine',
-    '-' => 'dash',
-    '/' => 'dot'
-  }
-
   def generate(username)
     s = Digest::SHA256
-    hash = s.base64digest(SALT + username).downcase
-    hash[0,8].split('').map { |c| MEMORABLE_WORDS[c] }.join(' ')
+    hash = s.hexdigest(SALT + username).to_i(16)
+    words = []
+
+    (1..8).each do
+      words << WORD_LIST[hash % WORD_LIST.length]
+      hash = hash / WORD_LIST.length
+    end
+
+    words.join(' ')
   end
 
   def authenticate?(username, password)
