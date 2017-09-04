5ab8f55597e944711d593a9e59ab5fc12267fa09
Toby Retallick <toby.retallick@gmail.com>
2017-09-04 16:42:40 +0100

    Set config.assets.compile to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,9 +28,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
-  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
+  # `config.assets.precompile` and `config.assets.version` have moved to configF/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
