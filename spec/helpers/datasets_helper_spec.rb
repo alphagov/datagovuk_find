af623d0534e9631eff9394664eaea492b2e63f71
Toby Retallick <toby.retallick@gmail.com>
2017-09-06 16:11:18 +0100

    Stub calls to publish API in feature tests

--- spec/helpers/datasets_helper_spec.rb
+++ spec/helpers/datasets_helper_spec.rb
@@ -20,24 +20,31 @@ describe DatasetsHelper, type: :helper do
   end
 
   it 'returns :no_preview if datafile has nil or blank format and no preview' do
-    stub_request(:any, 'https://good_data.co.uk').
-      to_return(status: 200)
+    stub_for_no_data_preview
 
     expect(helper.link_type(DATAFILES_WITHOUT_START_AND_ENDDATE[0])).to be :no_preview
   end
 
   it 'returns no_preview if datafile has no preview' do
-    stub_request(:any, 'https://good_data.co.uk').
-      to_return(status: 200)
+    stub_for_no_data_preview
 
     expect(helper.link_type(DATA_FILES_WITH_START_AND_ENDDATE[1])).to be :no_preview
   end
 
   it 'returns preview if datafile has a preview' do
-    stub_request(:any, 'https://find-data-beta.herokuapp.com/').
-      to_return(status: 200, body: [{data: 'blah'}])
+    stub_for_existing_data_preview
 
     expect(helper.link_type(CSV_DATAFILE)).to be :preview
+  end
+
+  def stub_for_no_data_preview
+    stub_request(:any, FETCH_PREVIEW_URL).
+      to_return(status: 200)
+  end
+
+  def stub_for_existing_data_preview
+    stub_request(:any, FETCH_PREVIEW_URL).
+      to_return(status: 200, body: [{data: 'blah'}])
   end
 
 end
