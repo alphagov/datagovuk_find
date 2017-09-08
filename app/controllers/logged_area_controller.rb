51557dfb461bef3aa855ceb464518bfece86b898
Laurent Curau <laurent.curau@digital.cabinet-office.gov.uk>
2017-09-08 11:06:27 +0100

    Extract consent into its own callback

--- app/controllers/logged_area_controller.rb
+++ app/controllers/logged_area_controller.rb
@@ -1,31 +1,38 @@
 require 'private_beta_user'
 
 class LoggedAreaController < ApplicationController
+  before_action :check_consent
   before_action :authenticate
-end
 
-def authenticate
-  return if Rails.env.development? || Rails.env.test?
-
-  if !session.key?('consent') || session[:consent] == false
-    redirect_to use_of_data_path and return
+  def check_consent
+    redirect_to use_of_data_path if has_not_consented?
   end
 
-  authenticate_or_request_with_http_basic('Please sign in with username and password provided to you') do |username, password|
-    httpauth_name = ENV['HTTP_USERNAME']
-    httpauth_pass = ENV['HTTP_PASSWORD']
+  def authenticate
+    return if Rails.env.development? || Rails.env.test?
 
-    admin_login = username == httpauth_name && password == httpauth_pass
-    beta_login = PrivateBetaUser.authenticate?(username, password)
+    authenticate_or_request_with_http_basic('Please sign in with username and password provided to you') do |username, password|
+      httpauth_name = ENV['HTTP_USERNAME']
+      httpauth_pass = ENV['HTTP_PASSWORD']
 
-    if beta_login
-      session[:beta_user] = username
+      admin_login = username == httpauth_name && password == httpauth_pass
+      beta_login = PrivateBetaUser.authenticate?(username, password)
+
+      if beta_login
+        session[:beta_user] = username
+      end
+
+      if admin_login == false && beta_login == false
+        redirect_to '/not_authenticated'
+      else
+        true
+      end
     end
+  end
 
-    if admin_login == false && beta_login == false
-      redirect_to '/not_authenticated'
-    else
-      true
-    end
+  private
+
+  def has_not_consented?
+    !session.key?('consent') || session[:consent] == false
   end
 end
