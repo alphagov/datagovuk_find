e607ffcd0a374ac2605daf36fc0789722a2f73df
Toby Retallick <toby.retallick@gmail.com>
2017-09-19 15:38:48 +0100

    Refactoring of Datasets Controller (#281)

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -3,45 +3,43 @@ class DatasetsController < LoggedAreaController
   include QueryBuilder
 
   def show
-    begin
-      query = get_query(name: params[:name])
-      @dataset = Dataset.get(query)
-      @timeseries_datafiles = @dataset.timeseries_datafiles
-      @non_timeseries_datafiles = @dataset.non_timeseries_datafiles
-      raise 'Metadata missing' if @dataset.title.blank?
-    rescue => e
-      handle_error(e)
-    end
-
-    @referrer = referrer
-
-    unless @dataset.nil?
-      query = related_to_query(@dataset._id)
-      @related_datasets = Dataset.related(query)
-    end
+    @dataset = dataset
+    @timeseries_datafiles = @dataset.timeseries_datafiles
+    @non_timeseries_datafiles = @dataset.non_timeseries_datafiles
+    @referer_query = referer_query
+    @related_datasets = related_datasets
+  rescue => e
+    handle_error(e)
   end
 
   private
 
+  def related_datasets
+    query = related_to_query(@dataset._id)
+    Dataset.related(query)
+  end
+
+  def dataset
+    query = get_query(name: params[:name])
+    dataset = Dataset.get(query)
+    raise 'Metadata missing' if dataset.title.blank?
+    dataset
+  end
+
   def handle_error(e)
-    Rails.logger.debug "ERROR! => " + e.message
-    e.backtrace.each {|line| logger.error line}
-    render :template => "errors/not_found", :status => 404
+    Rails.logger.debug 'ERROR! => ' + e.message
+    e.backtrace.each { |line| logger.error line }
+    render template: 'errors/not_found', status: 404
   end
 
-  def referrer
-    unless request.referer.nil?
-      referer_host = URI(request.referer).host.to_s
-      app_host = URI(request.host).to_s
-      referer_query = URI(request.referer).query
+  def referer_query
+    return if request.referer.nil?
+    referer_query = URI(request.referer).query
 
-      referer_query if referer_host == app_host
-    end
+    referer_query if current_host_matches_referer_host
   end
 
-  def request_dataset(preview)
-    dataset_id = preview['meta']['dataset_id']
-    query = get_query(id: dataset_id)
-    Dataset.get(query)
+  def current_host_matches_referer_host
+    URI(request.host).to_s == URI(request.referer).host.to_s 
   end
 end
