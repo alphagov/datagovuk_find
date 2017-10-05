027cf62d4ab43b03ada0a0081fbf47af7b65318f
Toby Retallick <toby.retallick@gmail.com>
2017-10-05 14:25:20 +0100

    Elasticsearch is run using PAAS or local configuration depending on the environment

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,16 +1,76 @@
-CONFIG_PATH = "#{Rails.root}/config/elasticsearch.yml"
-TEMPLATE = ERB.new File.new(CONFIG_PATH).read
-ELASTIC_CONFIG = YAML.load(TEMPLATE.result(binding))[ENV['RAILS_ENV']]
+require 'base64'
 
-config = {
-  host: ELASTIC_CONFIG['host'],
-  transport_options: {
-    request: {
-      timeout: 5
+CONFIG_PATH = Rails.root.join('config', 'elasticsearch.yml')
+TEMPLATE = ERB.new File.new(CONFIG_PATH).read
+
+begin
+  ELASTIC_CONFIG = YAML.load(TEMPLATE.result(binding))[ENV['RAILS_ENV']]
+rescue => e
+  Rails.logger.fatal 'Failed to parse elasticsearch yaml configuration. Exiting'
+  Rails.logger.fatal e
+  exit
+end
+
+def log(server, filepath)
+  Rails.logger.info "Configuring Elasticsearch on PAAS.\n
+  Elasticsearch host: #{server}\n
+  Elasticsearch cert file path: #{filepath}"
+end
+
+def create_es_cert_file(cert)
+  begin
+    es_cert_file = File.new('elasticsearch_cert.pem', 'w')
+    es_cert_file.write(cert)
+    es_cert_file.close
+  rescue => e
+    Rails.logger.fatal 'Failed to write elasticsearch certificate. Exiting'
+    Rails.logger.fatal e
+    exit
+  end
+  es_cert_file
+end
+
+def es_config_from_vcap
+  begin
+    vcap = JSON.parse(ELASTIC_CONFIG['vcap_services'])
+    es_server = vcap['elasticsearch'][0]['credentials']['uri'].chomp('/')
+    es_cert = Base64.decode64(vcap['elasticsearch'][0]['credentials']['ca_certificate_base64'])
+  rescue => e
+    Rails.logger.fatal 'Failed to extract ES creds from VCAP_SERVICES. Exiting'
+    Rails.logger.fatal ELASTIC_CONFIG['vcap_services']
+    Rails.logger.fatal e
+    exit
+  end
+  es_cert_file = create_es_cert_file(es_cert)
+  log(es_server, es_cert_file.path)
+
+  {
+    host: es_server,
+    transport_options: {
+      request: {
+        timeout: ELASTIC_CONFIG['elastic_timeout']
+      },
+      ssl: {
+        ca_file: es_cert_file.path
+      }
     }
-  },
-  send_get_body_as: 'POST'
-}
+  }
+end
+
+def es_config_from_host
+  {
+    host: ELASTIC_CONFIG['host'],
+    transport_options: {
+      request: {
+        timeout: ELASTIC_CONFIG['elastic_timeout']
+      }
+    }
+  }
+end
+
+config = ELASTIC_CONFIG['vcap_services'].present? ?
+  es_config_from_vcap :
+  es_config_from_host
 
 ELASTIC = Elasticsearch::Client.new(config)
 Elasticsearch::Model.client = ELASTIC
