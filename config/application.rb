929fda7059ca4df0cc1514c32b0214805763c63e
Laurence <laurence.smith@digital.cabinet-office.gov.uk>
2017-10-27 13:42:06 +0100

    Add Raven configs

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,12 @@ module FindDataBeta
     config.analytics_tracking_id = ENV['GA_TRACKING_ID']
     config.analytics_test_tracking_id = ENV['GA_TEST_TRACKING_ID']
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**','**','*.{rb,yml}')]
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.filter_parameters << :password
+    config.filter_parameters << :password_confirmation
+
+    Raven.configure do |config|
+      config.dsn = 'https://b9b9fe3fe0e84d95a2a2b513b7f859a7:e038e55809d64b2ba482e23031d0e21f@sentry.io/236517'
+      config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+    end
   end
 end
