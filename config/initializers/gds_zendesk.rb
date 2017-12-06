f30c1fe850fdd3ab32bca68658ed110da2dc4352
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2017-12-06 15:54:25 +0000

    read env variables from vcap services

--- config/initializers/gds_zendesk.rb
+++ config/initializers/gds_zendesk.rb
@@ -2,10 +2,37 @@ require 'yaml'
 require 'gds_zendesk/client'
 require 'gds_zendesk/dummy_client'
 
-GDS_ZENDESK_CLIENT = if Rails.env.development? || Rails.env.test?
-  GDSZendesk::DummyClient.new(logger: Rails.logger)
+ZENDESK_CONFIG_PATH = Rails.root.join('config', 'zendesk.yml')
+ZENDESK_TEMPLATE = ERB.new File.new(ZENDESK_CONFIG_PATH).read
+
+begin
+  ZENDESK_CONFIG = YAML.load(ZENDESK_TEMPLATE.result(binding))[ENV['RAILS_ENV']]
+rescue => e
+  Rails.logger.fatal 'Failed to parse zendesk yaml configuration. Exiting'
+  Rails.logger.fatal e
+  exit
+end
+
+def zendesk_config_from_vcap
+  config = {}
+  begin
+    vcap = JSON.parse(ZENDESK_CONFIG['vcap_services'])
+    config["username"] = vcap['user-provided'][0]['credentials']['ZENDESK_USERNAME']
+    config["password"] = vcap['user-provided'][0]['credentials']['ZENDESK_PASSWORD']
+  rescue => e
+    Rails.logger.fatal 'Failed to extract zendesk creds from VCAP_SERVICES. Exiting'
+    Rails.logger.fatal ZENDESK_CONFIG['vcap_services']
+    Rails.logger.fatal e
+  exit
+  end
+  config
+end
+
+username = zendesk_config_from_vcap['username']
+password = zendesk_config_from_vcap['password']
+
+GDS_ZENDESK_CLIENT = if Rails.env.production?
+  GDSZendesk::Client.new(username: username, password: password, logger: Rails.logger)
 else
-  config_yaml_file = File.join(Rails.root, 'config', 'zendesk.yml')
-  config = YAML.load_file(config_yaml_file)[Rails.env]
-  GDSZendesk::Client.new(username: config['username'], password: config['password'], logger: Rails.logger)
+  GDSZendesk::DummyClient.new(logger: Rails.logger)
 end
