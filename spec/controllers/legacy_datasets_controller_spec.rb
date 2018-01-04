6c7a082abe0a89efc5ef2a29dbc865ab02aa0d27
Laurent Curau <laurent.curau@digital.cabinet-office.gov.uk>
2018-01-04 11:34:57 +0000

    Fix test

--- spec/controllers/legacy_datasets_controller_spec.rb
+++ spec/controllers/legacy_datasets_controller_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 describe LegacyDatasetsController, type: :controller do
   describe 'visiting a dataset page with a legacy URL' do
     it "redirects to the latest slugged URL" do
-      dataset = DatasetBuilder.new.with_legacy_name('a-legacy-name').build
+      legacy_name = 'a-legacy-name'
+      dataset = DatasetBuilder.new.with_legacy_name(legacy_name).build
       index([dataset])
 
-      get :redirect, params: { legacy_name: 'a-legacy_name' }
+      get :redirect, params: { legacy_name: legacy_name }
 
       expect(response).to redirect_to(dataset_url(dataset[:uuid], dataset[:name]))
       expect(response.status).to eql 301
