3a1539c87a35eecf735eaa16ebeba76519422a8b
Laurent Curau <laurent.curau@digital.cabinet-office.gov.uk>
2018-01-05 11:10:43 +0000

    Refactor legacy redirects

--- spec/requests/legacy/redirect_spec.rb
+++ spec/requests/legacy/redirect_spec.rb
@@ -24,4 +24,17 @@ describe 'legacy', :type => :request do
       expect(response).to redirect_to(search_path(expected_params))
     end
   end
+
+  describe 'visiting a dataset page with a legacy URL' do
+    it "redirects to the latest slugged URL" do
+      legacy_name = 'a-legacy-name'
+      dataset = DatasetBuilder.new.with_legacy_name(legacy_name).build
+      index([dataset])
+
+      get "/dataset/#{legacy_name}"
+
+      expect(response).to redirect_to(dataset_url(dataset[:uuid], dataset[:name]))
+      expect(response.status).to eql 301
+    end
+  end
 end
