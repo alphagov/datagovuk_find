8ba233901336c0b6df465b9f95623c4eb0198d79
Laurent Curau <laurentcurau@gmail.com>
2018-02-14 15:55:11 +0000

    Dashboard of what data is present in the index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,6 +16,15 @@ class PagesController < ApplicationController
   def accessibility
   end
 
+  def dashboard
+    @datasets_count = Dataset.__elasticsearch__.client.count["count"]
+    @publishers_count = Dataset.publishers.count
+    @datafiles_count = Dataset.datafiles['doc_count']
+    @published_datasets_count = Dataset.datafiles['datasets_with_datafiles']['doc_count']
+    @unpublished_datasets_count = @datasets_count - @published_datasets_count
+    @datafiles_count_by_format = Dataset.datafiles['formats']['buckets']
+  end
+
   private
 
   def check_consent
@@ -26,5 +35,4 @@ class PagesController < ApplicationController
   def has_not_consented?
     !session.key?('consent') || session[:consent] == false
   end
-
 end
