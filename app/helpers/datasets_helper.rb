e1673c0db2c36c006818e4cb84b1b635b585d072
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2018-03-05 22:40:18 +0000

    extract more logic to dataset helpers

--- app/helpers/datasets_helper.rb
+++ app/helpers/datasets_helper.rb
@@ -72,11 +72,19 @@ module DatasetsHelper
   end
 
   def contact_information_exists?(dataset)
-    contact_email_exists?(dataset) || foi_email_exists?(dataset) || foi_web_address_exists?(dataset)
+    contact_email_exists?(dataset) || foi_details_exist?(dataset)
   end
 
   def contact_email_exists?(dataset)
     dataset.contact_email.present? || dataset.organisation.contact_email.present?
+  end
+
+  def contact_name_for(dataset)
+    dataset.contact_name.presence || dataset.organisation.contact_name
+  end
+
+  def contact_email_for(dataset)
+    dataset.contact_email.presence || dataset.organisation.contact_email
   end
 
   def foi_details_exist?(dataset)
@@ -89,5 +97,17 @@ module DatasetsHelper
 
   def foi_web_address_exists?(dataset)
     dataset.foi_web.present? || dataset.organisation.foi_web.present?
+  end
+
+  def foi_name_for(dataset)
+    dataset.foi_name.presence || dataset.organisation.foi_name
+  end
+
+  def foi_email_for(dataset)
+    dataset.foi_email.presence || dataset.organisation.foi_email
+  end
+
+  def foi_web_address_for(dataset)
+    dataset.foi_web.presence || dataset.organisation.foi_web
   end
 end
