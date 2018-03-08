34be16f897ba056a0e5ca6d3b8e7761f87360686
Andrew Garner <andrew@andrewgarner.com>
2018-03-08 10:58:54 +0000

    Correctly handle requesting missing datafiles
    Raise an handle an exception if the datafile can't be found.¬

--- app/controllers/previews_controller.rb
+++ app/controllers/previews_controller.rb
@@ -2,5 +2,7 @@ class PreviewsController < ApplicationController
   def show
     @dataset = Dataset.get_by_uuid(uuid: params[:dataset_uuid])
     @datafile = @dataset.datafiles.find { |f| f.uuid == params[:datafile_uuid] }
+
+    raise Datafile::DatafileNotFound if @datafile.nil?
   end
 end
--- spec/requests/error_handling_spec.rb
+++ spec/requests/error_handling_spec.rb
@@ -13,4 +13,22 @@ RSpec.describe 'Error handling', type: :request do
 
     expect(response).to have_http_status(:not_found)
   end
+
+  it "handles a Datafile that can't be found by UUID" do
+    dataset_name = 'my-dataset'
+    dataset_uuid = SecureRandom.uuid
+    datafile_uuid = SecureRandom.uuid
+
+    dataset = DatasetBuilder
+                .new
+                .with_name(dataset_name)
+                .with_uuid(dataset_uuid)
+                .build
+
+    index(dataset)
+
+    get "/dataset/#{dataset_uuid}/#{dataset_name}/datafile/#{datafile_uuid}/preview"
+
+    expect(response).to have_http_status(:not_found)
+  end
 end
