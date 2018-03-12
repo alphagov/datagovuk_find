04b3d3c03fc7b960b14a6aec10cacd24e6a1b387
Andrew Garner <andrew@andrewgarner.com>
2018-03-12 08:43:24 +0000

    Move Sentry config to initialiser

--- config/application.rb
+++ config/application.rb
@@ -38,13 +38,5 @@ module FindDataBeta
     config.filter_parameters << :password_confirmation
 
     config.elasticsearch = config_for(:elasticsearch)
-
-    Raven.configure do |config|
-      if ENV['SENTRY_DSN']
-        config.dsn = ENV['SENTRY_DSN']
-      end
-
-      config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-    end
   end
 end
