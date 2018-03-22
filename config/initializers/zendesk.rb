6a627162d22fa4d983b123d69ba9c40d10212f70
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2018-03-22 16:54:36 +0000

    Improve logging of error messages around zendesk configuration. Don't blow up if zendesk credentials aren't available, load app with a dummy client.

--- config/initializers/zendesk.rb
+++ config/initializers/zendesk.rb
@@ -5,18 +5,15 @@ def zendesk_config_from_vcap
   begin
     vcap = JSON.parse(ENV["VCAP_SERVICES"])
     vcap['user-provided'].each do |elem|
-      if (elem.has_key? 'credentials') && (elem['credentials'].has_key? 'ZENDESK_USERNAME')
-        zendesk_config["username"] = elem['credentials']['ZENDESK_USERNAME']
-        zendesk_config["password"] = elem['credentials']['ZENDESK_PASSWORD']
-        zendesk_config["api_key"] = elem['credentials']['ZENDESK_API_KEY']
-        zendesk_config["end_point"] = elem['credentials']['ZENDESK_END_POINT']
-      end
+      zendesk_config["username"] = elem['credentials']['ZENDESK_USERNAME']
+      zendesk_config["password"] = elem['credentials']['ZENDESK_PASSWORD']
+      zendesk_config["api_key"] = elem['credentials']['ZENDESK_API_KEY']
+      zendesk_config["end_point"] = elem['credentials']['ZENDESK_END_POINT']
     end
   rescue => e
-    Rails.logger.fatal 'Failed to extract zendesk creds from VCAP_SERVICES. Exiting'
-    Rails.logger.fatal vcap
-    Rails.logger.fatal e
-    raise e
+    Rails.logger.info "Failed to extract zendesk creds from VCAP_SERVICES. RAILS_ENV = #{ENV['RAILS_ENV']}"
+    Rails.logger.info "VCAP_SERVICES: '#{vcap}'"
+    Rails.logger.info "ERROR --> '#{e}'"
   end
   zendesk_config
 end
@@ -31,12 +28,19 @@ def build_zendesk_client(username, password, api_key, end_point)
 end
 
 if Rails.env.production?
-  config = zendesk_config_from_vcap
-  username = config['username']
-  password = config['password']
-  api_key = config['api_key']
-  end_point = config['end_point']
-  GDS_ZENDESK_CLIENT = build_zendesk_client(username, password, api_key, end_point)
+  begin
+    config = zendesk_config_from_vcap
+    username = config['username']
+    password = config['password']
+    api_key = config['api_key']
+    end_point = config['end_point']
+    GDS_ZENDESK_CLIENT = build_zendesk_client(username, password, api_key, end_point)
+  rescue => e
+    Rails.logger.info "Failed to initialise zendesk client. Using dummy client"
+    Rails.logger.info "zendesk config = '#{config}'"
+    Rails.logger.info "ERROR --> '#{e}'"
+    GDS_ZENDESK_CLIENT = ZendeskDummyClient.new
+  end
 end
 
 if Rails.env.test?
