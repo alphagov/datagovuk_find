ecca02e1f9445d229530bcac944e385938eb79b1
Andrew Garner <andrew@andrewgarner.com>
2018-03-29 15:15:41 +0100

    Update redirect spec for new content

--- spec/requests/legacy/dataset_creation_spec.rb
+++ spec/requests/legacy/dataset_creation_spec.rb
@@ -5,7 +5,7 @@ describe 'legacy', type: :request do
     it 'shows a page informing the user the newly created dataset will be available soon' do
       get '/dataset/foo-bar', params: {}, headers: { 'HTTP_REFERER' => "http://foobar.com/dataset/new" }
 
-      expect(response.body).to include("Available Soon")
+      expect(response.body).to include("Changes to your datasets")
     end
   end
 end
