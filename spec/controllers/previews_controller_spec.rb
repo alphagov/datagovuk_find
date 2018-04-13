98db7bb1d67f1e7f5e3ee59686fd5ea049ef2f96
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2018-04-13 07:39:12 +0100

    update specs to check for download link

--- spec/controllers/previews_controller_spec.rb
+++ spec/controllers/previews_controller_spec.rb
@@ -14,58 +14,63 @@ describe PreviewsController, type: :controller do
 
   describe 'Generating the preview of a CSV file' do
 
-    before(:each) do
-      NOT_AVAILABLE = "Currently there is no preview available for \"#{datafile.name}\""
-    end
+  before(:each) do
+    NOT_AVAILABLE = "Currently there is no preview available for \"#{datafile.name}\""
+  end
 
-    it 'will show the previewed CSV' do
-      stub_request(:get, datafile.url).
-        to_return(body: "a,Paris,c,d\ne,f,Berlin,h\ni,j,k,l")
+  it 'will show the previewed CSV' do
+    stub_request(:get, datafile.url).
+      to_return(body: "a,Paris,c,d\ne,f,Berlin,h\ni,j,k,l")
 
-      index([dataset])
-      get :show, params: { dataset_uuid: dataset[:uuid], name: dataset[:name], datafile_uuid: datafile.uuid }
-      expect(response.body).to have_content('Berlin')
-    end
+    index([dataset])
+    get :show, params: { dataset_uuid: dataset[:uuid], name: dataset[:name], datafile_uuid: datafile.uuid }
+    expect(response.body).to have_content('Berlin')
+    expect(response.body).to have_link("Download this file")
+  end
 
-    it 'will recover if the datafile server times out' do
-      stub_request(:get, datafile.url).
-        to_timeout
+  it 'will recover if the datafile server times out' do
+    stub_request(:get, datafile.url).
+      to_timeout
 
-      index([dataset])
-      get :show, params: { dataset_uuid: dataset[:uuid], name: dataset[:name], datafile_uuid: datafile.uuid }
+    index([dataset])
+    get :show, params: { dataset_uuid: dataset[:uuid], name: dataset[:name], datafile_uuid: datafile.uuid }
 
-      expect(response.body).to have_content(NOT_AVAILABLE)
-    end
+    expect(response.body).to have_content(NOT_AVAILABLE)
+    expect(response.body).not_to have_link("Download this file")
+  end
 
-    it 'will recover if the datafile server returns an error' do
-      stub_request(:get, datafile.url).
-        to_return(status: [500, "Internal Server Error"])
+  it 'will recover if the datafile server returns an error' do
+    stub_request(:get, datafile.url).
+      to_return(status: [500, "Internal Server Error"])
 
-      index([dataset])
-      get :show, params: { dataset_uuid: dataset[:uuid], name: dataset[:name], datafile_uuid: datafile.uuid }
+    index([dataset])
+    get :show, params: { dataset_uuid: dataset[:uuid], name: dataset[:name], datafile_uuid: datafile.uuid }
 
-      expect(response.body).to have_content(NOT_AVAILABLE)
-    end
+    expect(response.body).to have_content(NOT_AVAILABLE)
+    expect(response.body).not_to have_link("Download this file")
+  end
 
-    it 'will recover if the datafile is not CSV' do
-      stub_request(:get, datafile.url).
-        to_return(body: "<!DOCTYPE html><html lang=\"en\"><h")
+  it 'will recover if the datafile is not CSV' do
+    stub_request(:get, datafile.url).
+      to_return(body: "<!DOCTYPE html><html lang=\"en\"><h")
 
-      index([dataset])
-      get :show, params: { dataset_uuid: dataset[:uuid], name: dataset[:name], datafile_uuid: datafile.uuid }
+    index([dataset])
+    get :show, params: { dataset_uuid: dataset[:uuid], name: dataset[:name], datafile_uuid: datafile.uuid }
 
-      expect(response.body).to have_content(NOT_AVAILABLE)
-    end
+    expect(response.body).to have_content(NOT_AVAILABLE)
+    expect(response.body).not_to have_link("Download this file")
+  end
 
-    it 'will recover if the datafile is malformed CSV' do
-      stub_request(:get, datafile.url).
-        to_return(body: "a,b,\",c,d\n000000\n")
+  it 'will recover if the datafile is malformed CSV' do
+    stub_request(:get, datafile.url).
+      to_return(body: "a,b,\",c,d\n000000\n")
 
-      index([dataset])
-      get :show, params: { dataset_uuid: dataset[:uuid], name: dataset[:name], datafile_uuid: datafile.uuid }
+    index([dataset])
+    get :show, params: { dataset_uuid: dataset[:uuid], name: dataset[:name], datafile_uuid: datafile.uuid }
 
-      expect(response.body).to have_content(NOT_AVAILABLE)
-    end
+    expect(response.body).to have_content(NOT_AVAILABLE)
+    expect(response.body).not_to have_link("Download this file")
+  end
 
   end
 end
