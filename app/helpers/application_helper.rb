2d3e71b6cf1248c205d73497ff11b84cca8a6a20
Laurent Curau <laurentcurau@gmail.com>
2018-04-16 12:44:06 +0100

    Create IE detection helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def format_timestamp(timestamp)
     Time.parse(timestamp).strftime('%d %B %Y')
   end
+
+  def browser
+    Browser.new(request.headers['HTTP_USER_AGENT'])
+  end
 end
---
+++ app/models/browser.rb
@@ -0,0 +1,22 @@
+class Browser
+  attr_reader :user_agent
+
+  def initialize(user_agent)
+    @user_agent = user_agent
+  end
+
+  def ie?(version:)
+    ie_version.to_i == version.to_i
+  end
+
+  private
+
+  def ie_version
+    ie_full_version.split(".").first
+  end
+
+  def ie_full_version
+    (user_agent.match(%r{MSIE ([\d.]+)}) && ($1)) ||
+      "0.0"
+  end
+end
