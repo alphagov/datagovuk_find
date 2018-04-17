b141f64680b2b4991a302d22c1a335ec3657aa30
Laurent Curau <laurent.curau@digital.cabinet-office.gov.uk>
2018-04-17 16:05:44 +0100

    Require rails-ujs instead of jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require rails-ujs
 //= require govuk_toolkit
 //= require accessible-autocomplete/dist/accessible-autocomplete.min
 //= require_tree .
