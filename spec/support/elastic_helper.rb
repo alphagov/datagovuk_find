d13a57a3b2ab5fa66682e078c27484246d7654f9
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-04-25 09:16:53 +0100

    Move elastic helpers into helper file

---
+++ spec/support/elastic_helper.rb
@@ -0,0 +1,162 @@
+def delete_index
+  ELASTIC.indices.delete index: "datasets-test"
+rescue
+  Rails.logger.debug("No test search index to delete")
+end
+
+def create_index
+  Rails.logger.info("Creating datasets-test index")
+
+  ELASTIC.indices.create(
+    index: "datasets-test",
+    body: {
+      settings: index_settings,
+      mappings: index_mappings
+    }
+  )
+rescue
+  Rails.logger.debug("Could not create datasets-test index")
+end
+
+def index_settings
+  {
+    number_of_shards: 1, # necessary so our relevance specs work (more info: https://www.elastic.co/guide/en/elasticsearch/guide/current/relevance-is-broken.html)
+    analysis: {
+      normalizer: {
+        lowercase_normalizer: {
+          type: "custom",
+          filter: "lowercase"
+        }
+      }
+    }
+  }
+end
+
+def index_mappings
+  {
+    dataset: {
+      properties: {
+        name: {
+          type: "keyword",
+          index: true,
+        },
+        title: {
+          type: 'text',
+          fields: {
+            keyword: {
+              type: 'keyword',
+              index: true,
+            },
+            english: {
+              type: 'text',
+              analyzer: 'english',
+            },
+          },
+        },
+        summary: {
+          type: 'text',
+          fields: {
+            keyword: {
+              type: 'keyword',
+              index: true,
+            },
+            english: {
+              type: 'text',
+              analyzer: 'english',
+            },
+          },
+        },
+        description: {
+          type: 'text',
+          fields: {
+            keyword: {
+              type: 'keyword',
+              index: true,
+            },
+            english: {
+              type: 'text',
+              analyzer: 'english',
+            },
+          },
+        },
+        legacy_name: {
+          type: "keyword",
+          index: true,
+        },
+        uuid: {
+          type: "keyword",
+          index: true,
+        },
+        location1: {
+          type: 'text',
+          fields: {
+            raw: {
+              type: "keyword",
+              index: true,
+            }
+          }
+        },
+        topic: {
+          type: 'nested',
+          properties: {
+            title: {
+              type: 'text',
+              fields: {
+                raw: {
+                  type: "keyword",
+                  index: true,
+                }
+              }
+            }
+          }
+        },
+        organisation: {
+          type: "nested",
+          properties: {
+            title: {
+              type: "text",
+              fields: {
+                raw: {
+                  type: "keyword",
+                  index: true,
+                },
+                english: {
+                  type: 'text',
+                  analyzer: 'english',
+                }
+              }
+            },
+            description: {
+              type: 'text',
+              fields: {
+                raw: {
+                  type: 'keyword',
+                  index: true,
+                },
+                english: {
+                  type: 'text',
+                  analyzer: 'english',
+                },
+              },
+            },
+          }
+        },
+        datafiles: {
+          type: "nested",
+          properties: {
+            format: {
+              type: "keyword",
+              normalizer: "lowercase_normalizer"
+            }
+          }
+        },
+        docs: {
+          type: "nested",
+          properties: {
+            format: { type: "keyword" }
+          }
+        }
+      }
+    }
+  }
+end
