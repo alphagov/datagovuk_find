89a33900c34bfee6417ca50fa54b430be2c0c2a5
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-05-01 10:25:51 +0100

    Fix stray linting errors

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,14 +1,14 @@
 SecureHeaders::Configuration.default do |config|
   config.csp = {
     preserve_schemes: true,
-    default_src: %w('none'),
-    connect_src: %w('self' www.google-analytics.com),
-    font_src: %w('self' data:),
-    img_src: %w('self' www.google-analytics.com),
-    manifest_src: %w('self'),
-    media_src: %w('self'),
-    object_src: %w('self'),
-    script_src: %w('unsafe-inline' 'self' www.google-analytics.com),
-    style_src: %w('unsafe-inline' 'self')
+    default_src: ["'none'"],
+    connect_src: ["'self'", "www.google-analytics.com"],
+    font_src: ["'self'", "data:"],
+    img_src: ["'self'", "www.google-analytics.com"],
+    manifest_src: ["'self'"],
+    media_src: ["'self'"],
+    object_src: ["'self'"],
+    script_src: ["'unsafe-inline'", "'self'", "www.google-analytics.com"],
+    style_src: ["'unsafe-inline'", "'self'"]
   }
 end
