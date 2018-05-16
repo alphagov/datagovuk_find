5d4c6bca87d684fe671c80ba814f0eb0101cc429
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-05-16 15:05:46 +0100

    Remove redundant rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,9 @@ require "simplecov"
 require 'webmock/rspec'
 
 SimpleCov.start
-
 WebMock.disable_net_connect!(allow_localhost: true)
 
 RSpec.configure do |config|
-  config.filter_run focus: true
-  config.run_all_when_everything_filtered = true
-
   config.before(:each) do
     delete_index
     create_index
@@ -18,21 +14,6 @@ RSpec.configure do |config|
     delete_index
   end
 
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
-
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
-  end
-
-  config.shared_context_metadata_behavior = :apply_to_host_groups
-
-  if config.files_to_run.one?
-    config.default_formatter = "doc"
-  end
-
-  config.profile_examples = 1
   config.order = :random
   Kernel.srand config.seed
 end
