8ba9eae5845e49d840ddcce5d06b33c361b6f8d5
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-05-16 11:51:29 +0100

    Revert "Disable sessions in favour of manual cookies"
    This reverts commit ef59e515f4bf85842054e82997460714344a54ff.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :disabled
+Rails.application.config.session_store :cookie_store, expire_after: 14.days, secure: !(Rails.env.development? || Rails.env.test?), httponly: true
