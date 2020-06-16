6b42a109357124f5f14dec95a745d0835f32a860
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2020-06-16 16:36:13 +0100

    Update files to enable running of tests in Docker

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,7 +33,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
 
 FactoryBot.find_definitions
 
-WebMock.disable_net_connect!(allow_localhost: true)
+WebMock.disable_net_connect!(allow_localhost: true, allow: "elasticsearch")
 
 RSpec.configure do |config|
   # RSpec Rails can automatically mix in different behaviours to your tests
--- spec/support/govuk_test.rb
+++ spec/support/govuk_test.rb
@@ -1,1 +1,1 @@
-GovukTest.configure(chrome_options: { w3c: false })
+GovukTest.configure(chrome_options: { w3c: false, args: ["--no-sandbox", "--headless"] })
