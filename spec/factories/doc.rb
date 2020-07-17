ce074bfa7e541802a8058d6e1440882ef8914e6e
pudinha <rogi@skylittlesystem.org>
2020-07-17 18:10:28 +0100

    Update usage of FactoryBot

--- spec/factories/doc.rb
+++ spec/factories/doc.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :doc do
-    name "My Doc"
-    url "http://example.com"
-    format ""
-    uuid SecureRandom.uuid
-    created_at Time.now.iso8601
+    name { "My Doc" }
+    url { "http://example.com" }
+    format { "" }
+    uuid { SecureRandom.uuid }
+    created_at { Time.now.iso8601 }
 
     trait :raw do
       initialize_with { attributes.stringify_keys }
