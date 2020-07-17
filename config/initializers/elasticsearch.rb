a8840774f26afd3bb084d2133f31d4ada71231d6
pudinha <rogi@skylittlesystem.org>
2020-07-17 16:10:10 +0100

    Fix deprecated string access in config_for hashes
    See: https://github.com/rails/rails/pull/35198

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,10 +1,10 @@
 es_config = Rails.configuration.elasticsearch
 
-host = es_config["host"]
+host = es_config[:host]
 
 if host.blank?
-  vcap = JSON.parse(es_config["vcap_services"])
-  host = vcap.dig("elasticsearch", 0, "credentials", "uri")
+  vcap = JSON.parse(es_config[:vcap_services])
+  host = vcap.dig(:elasticsearch, 0, :credentials, :uri)
 end
 
 raise StandardError.new("No elasticsearch environment variables found") if host.blank?
@@ -13,10 +13,10 @@ Elasticsearch::Model.client = Elasticsearch::Client.new(
   host: host,
   transport_options: {
     request: {
-      timeout: es_config.fetch("elastic_timeout"),
+      timeout: es_config.fetch(:elastic_timeout),
     },
     ssl: {
-      verify: es_config.fetch("verify_ssl", true),
+      verify: es_config.fetch(:verify_ssl, true),
     },
   },
 )
