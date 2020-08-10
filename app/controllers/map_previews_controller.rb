b68b35267bf776a0f3d9540e7521c50a2dfd3bbf
Edward Kerry <edward.kerry@digital.cabinet-office.gov.uk>
2020-08-10 09:33:06 +0100

    Correct Rubocop Style/PercentLiteralDelimiters errors

--- app/controllers/map_previews_controller.rb
+++ app/controllers/map_previews_controller.rb
@@ -6,12 +6,12 @@ class MapPreviewsController < ApplicationController
   def show
     # rubocop:disable Lint/PercentStringArray
     append_content_security_policy_directives(
-      script_src: %w(osinspiremappingprod.ordnancesurvey.co.uk 'unsafe-eval'),
-      style_src: %w(osinspiremappingprod.ordnancesurvey.co.uk),
+      script_src: %w[osinspiremappingprod.ordnancesurvey.co.uk 'unsafe-eval'],
+      style_src: %w[osinspiremappingprod.ordnancesurvey.co.uk],
     )
     # rubocop:enable Lint/PercentStringArray
 
-    override_content_security_policy_directives(img_src: %w(* data:))
+    override_content_security_policy_directives(img_src: %w[* data:])
   end
 
   def getinfo
@@ -56,7 +56,7 @@ private
     args["request"] ||= params.fetch("request", "GetCapabilities")
     args["service"] = "WMS"
 
-    if %w(getcapabilities getfeatureinfo).exclude?(args["request"].downcase)
+    if %w[getcapabilities getfeatureinfo].exclude?(args["request"].downcase)
       raise "Invalid request value for #{uri}"
     end
 
