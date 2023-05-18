5445fa65f486a98a53c753002c43bb008f1d5853
kentsang.gds <ken.tsang@digital.cabinet-office.gov.uk>
2023-05-18 19:41:52 +0100

    f

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -78,10 +78,10 @@ Capybara.server = :puma, { Silent: true }
 Capybara.register_driver :chrome_headless do |app|
   options = ::Selenium::WebDriver::Chrome::Options.new
 
-  options.add_argument('--headless')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
-  options.add_argument('--window-size=1400,1400')
+  options.add_argument("--headless")
+  options.add_argument("--no-sandbox")
+  options.add_argument("--disable-dev-shm-usage")
+  options.add_argument("--window-size=1400,1400")
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
