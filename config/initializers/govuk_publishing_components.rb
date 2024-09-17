51c6e9fad7af540af669d661980958f13bc830ac
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2024-09-17 11:23:30 +0100

    Upgrade govuk_publishing_components
    - upgrade to last version before the more complex switch to govuk-frontend v5
    - update manifest files and Sass to compile all CSS into one file
    - remove print stylesheet as no longer relevant
    - update Sass variables/mixins for newer govuk-frontend

--- config/initializers/govuk_publishing_components.rb
+++ config/initializers/govuk_publishing_components.rb
@@ -2,6 +2,5 @@ GovukPublishingComponents.configure do |c|
   c.component_guide_title = "Data.gov.uk component guide"
 
   c.application_stylesheet = "application"
-  c.application_print_stylesheet = "print"
   c.application_javascript = "application"
 end
