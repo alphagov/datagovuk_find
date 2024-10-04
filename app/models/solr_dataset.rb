9e95f74d80802f32f3f7b8c1daaf78ea072f3791
D Chua <deborah.chua@digital.cabinet-office.gov.uk>
2024-10-04 17:23:29 +0100

    Add meta information to dataset page

--- app/models/solr_dataset.rb
+++ app/models/solr_dataset.rb
@@ -3,9 +3,16 @@ class SolrDataset
 
   DatasetNotFound = Class.new(StandardError)
 
-  attr_reader :title
+  attr_reader :id, :name, :title, :public_updated_at, :licence_title, :organisation
 
   def initialize(dataset)
+    @id = dataset["id"]
+    @name = dataset["name"]
     @title = dataset["title"]
+    @public_updated_at = dataset["metadata_modified"]
+
+    dataset_dict = JSON.parse(dataset["validated_data_dict"])
+    @licence_title = dataset_dict["license_title"]
+    @organisation = dataset_dict["organization"]
   end
 end
