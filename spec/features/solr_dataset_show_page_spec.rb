f3ea0260446972043a62f534f32dfb662a6a6ff8
D Chua <deborah.chua@digital.cabinet-office.gov.uk>
2024-10-07 14:01:40 +0100

    Add metadata box to dataset page
    This commit shows the publisher, last updated and summary in the
    metadata box. If the topic and licence information is available
    they will also be included.

--- spec/features/solr_dataset_show_page_spec.rb
+++ spec/features/solr_dataset_show_page_spec.rb
@@ -31,4 +31,22 @@ RSpec.feature "Solr Dataset page", type: :feature do
         .to have_css('meta[name="dc:rights"][content="UK Open Government Licence (OGL)"]', visible: false)
     end
   end
+
+  feature "Metadata box" do
+    scenario "Displays the publisher name" do
+      expect(page).to have_content("Published by: Ministry of Housing, Communities and Local Government")
+    end
+
+    scenario "'Last updated' field displays public_updated_at" do
+      expect(page).to have_content("Last updated: 30 June 2017")
+    end
+
+    scenario "Displays the topic if there is one" do
+      expect(page).to have_content("Topic: Government")
+    end
+
+    scenario "Displays the summary" do
+      expect(page).to have_content("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod mauris in augue laoreet congue. Phasellus bibendum leo vel magna lacinia eleifend. Nam vitae lectus quis nulla varius faucibus id quis nibh. Nullam auctor ipsum non nunc efficitur bibendum Sed vitae ex nisi. Suspendisse posuere purus ac dui posuere, in interdum risus ornare.\nThe following files can be found on the website here:\nhttps://www.gov.uk/")
+    end
+  end
 end
