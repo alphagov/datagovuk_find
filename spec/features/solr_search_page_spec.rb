7ce1eabc7ddbf2dafbd927539b8a495d04a3c5fa
D Chua <deborah.chua@digital.cabinet-office.gov.uk>
2024-10-22 15:03:58 +0100

    Add non working filters on results page
    We want to filter by:
    - publisher (organisation)
    - topic
    - format
    - whether a dataset uses the OGL licence
    
    Users can also reset all filters.
    
    Functionality of the individual filters will be added in future commits.

--- spec/features/solr_search_page_spec.rb
+++ spec/features/solr_search_page_spec.rb
@@ -6,31 +6,27 @@ RSpec.describe "Solr Search page", type: :feature do
   describe "Default search page behaviour" do
     before do
       allow(Search::Solr).to receive(:search).and_return(JSON.parse(results))
+      visit "/search/solr"
     end
 
     scenario "Displays search heading" do
-      visit "/search/solr"
       expect(page).to have_css("h1", text: "Search results")
     end
 
     scenario "Displays search box" do
-      visit "/search/solr"
       expect(page).to have_content("Search data.gov.uk")
     end
 
     scenario "Displays search results" do
-      visit "/search/solr"
       expect(page).to have_content("2 results found")
     end
 
     scenario "Displays the title for each search result" do
-      visit "/search/solr"
       expect(page).to have_css("h2", text: "A very interesting dataset")
       expect(page).to have_css("h2", text: "A dataset with additional inspire metadata")
     end
 
     scenario "Displays the publisher for each search result" do
-      visit "/search/solr"
       results = all(".published_by")
       expect(results.length).to be(2)
       expect(results[0]).to have_content "Ministry of Housing, Communities and Local Government"
@@ -38,15 +34,46 @@ RSpec.describe "Solr Search page", type: :feature do
     end
 
     scenario "Displays the last updated for each search result" do
-      visit "/search/solr"
       expect(page).to have_content("Last updated", count: 2)
       expect(page).to have_content("30 June 2017")
       expect(page).to have_content("17 August 2018")
     end
 
     scenario "Results are sorted by best match" do
-      visit "/search/solr"
       expect(page).to have_select("sort", selected: "Best match")
+    end
+
+    describe "Filters" do
+      scenario "Displays the 'Filter by' heading" do
+        expect(page).to have_css("h2", text: "Filter by")
+      end
+
+      scenario "Displays the 'Publisher' filter" do
+        expect(page).to have_css(".dgu-filters .govuk-form-group", text: "Publisher")
+        expect(page).to have_css("select#publisher")
+      end
+
+      scenario "Displays the 'Topic' filter" do
+        expect(page).to have_css(".dgu-filters .govuk-form-group", text: "Topic")
+        expect(page).to have_css("select#topic")
+      end
+
+      scenario "Displays the 'Format' filter" do
+        expect(page).to have_css(".dgu-filters .govuk-form-group", text: "Format")
+        expect(page).to have_css("select#format")
+      end
+
+      scenario "Displays the 'OGL' filter" do
+        expect(page).to have_css(".dgu-filters .govuk-form-group", text: "Open Government Licence (OGL) only")
+      end
+
+      scenario "Displays the 'Apply filters' button" do
+        expect(page).to have_css("button", text: "Apply filters")
+      end
+
+      scenario "Displays the 'Remove filters' option" do
+        expect(page).to have_link("Remove filters", href: "/search/solr")
+      end
     end
   end
 
