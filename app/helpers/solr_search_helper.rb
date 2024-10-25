c4293c8b588fac0525dba6cc936530c2cd3ead60
D Chua <deborah.chua@digital.cabinet-office.gov.uk>
2024-10-25 15:58:46 +0100

    List all topics in topic filter
    This is the default list shown when a user visits the search page
    without entering a search term of selected any filters.
    
    We can filter by a selected topic by updating the Solr query so
    that is searches for the topic in the `extras_theme-primary` field.

--- app/helpers/solr_search_helper.rb
+++ app/helpers/solr_search_helper.rb
@@ -2,4 +2,27 @@ module SolrSearchHelper
   def selected_publisher
     params.dig(:filters, :publisher)
   end
+
+  def selected_topic
+    params.dig(:filters, :topic)
+  end
+
+  def solr_dataset_topics_for_select
+    [
+      "Business and economy",
+      "Crime and justice",
+      "Defence",
+      "Digital service performance",
+      "Education",
+      "Environment",
+      "Government",
+      "Government reference data",
+      "Government spending",
+      "Health",
+      "Mapping",
+      "Society",
+      "Towns and cities",
+      "Transport",
+    ].freeze
+  end
 end
