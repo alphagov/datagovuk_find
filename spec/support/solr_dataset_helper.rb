529d6b2699342044dd6b494f022de10d9327cfd1
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2024-11-04 13:29:10 +0000

    Update filtered_solr_search_for
    Scope searches to the correct elements.

--- spec/support/solr_dataset_helper.rb
+++ spec/support/solr_dataset_helper.rb
@@ -1,7 +1,9 @@
 def filtered_solr_search_for(sort_method)
   visit "/search/solr"
+  within ".dgu-sort" do
+    select sort_method, from: "sort-datasets"
+  end
   within "#main-content" do
-    select sort_method, from: "Sort by"
     find(".gem-c-search__submit").click
   end
 end
