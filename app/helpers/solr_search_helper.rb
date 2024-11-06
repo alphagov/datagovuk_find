830341f39f7b977e1a5e7e813982c25d5855c6c4
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2024-11-06 16:03:13 +0000

    REFACTOR: Move #selected_publisher and #selected_topic to presenter

--- app/helpers/solr_search_helper.rb
+++ app/helpers/solr_search_helper.rb
@@ -1,12 +1,4 @@
 module SolrSearchHelper
-  def selected_publisher
-    params.dig(:filters, :publisher)
-  end
-
-  def selected_topic
-    params.dig(:filters, :topic)
-  end
-
   def selected_format
     params.dig(:filters, :format)
   end
