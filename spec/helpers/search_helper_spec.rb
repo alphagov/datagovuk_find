76bd3ec5e1bfc7c3bb1110f2a2f264477ba858ff
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2024-12-16 16:50:13 +0000

    Add SearchHelper.process_query to remove stop words
    We want to remove popular stop words so the search performs closer to the
    OpenSearch English analyzer.  Opted to hardcode them rather than import from a
    third part source, as they are unlikely to change (also added &). Reducing
    reliance on third party integrations will improve reliability.
    
    The Regex is used to extract phrases (inside quotes) and other terms.  Then we
    separate phrases and terms and remove stop words from standalone terms (leaving
    phases intact).

--- spec/helpers/search_helper_spec.rb
+++ spec/helpers/search_helper_spec.rb
@@ -1,6 +1,53 @@
 require "rails_helper"
 
 RSpec.describe SearchHelper do
+  describe "#process_query" do
+    it "removes stop words from terms but keeps phrases intact" do
+      query_params = '"quick brown" fox jumps over "lazy dog" in the forest'
+      expected_result = '"quick brown" fox jumps "lazy dog" forest'
+
+      result = SearchHelper.process_query(query_params)
+
+      expect(result).to eq(expected_result)
+    end
+
+    it "handles input with only terms" do
+      query_params = "fox jumps over the lazy dog"
+      expected_result = "fox jumps lazy dog"
+
+      result = SearchHelper.process_query(query_params)
+
+      expect(result).to eq(expected_result)
+    end
+
+    it "handles input with only phrases" do
+      query_params = '"quick brown" "lazy dog"'
+      expected_result = '"quick brown" "lazy dog"'
+
+      result = SearchHelper.process_query(query_params)
+
+      expect(result).to eq(expected_result)
+    end
+
+    it "removes multiple stop words while preserving meaningful terms" do
+      query_params = "a quick and simple test of the analyzer"
+      expected_result = "quick simple test analyzer"
+
+      result = SearchHelper.process_query(query_params)
+
+      expect(result).to eq(expected_result)
+    end
+
+    it "handles empty input gracefully" do
+      query_params = ""
+      expected_result = ""
+
+      result = SearchHelper.process_query(query_params)
+
+      expect(result).to eq(expected_result)
+    end
+  end
+
   describe "#datafile_formats_for_select" do
     let(:dataset1) do
       build :dataset, datafiles: [build(:datafile, :raw, format: "foo"),
