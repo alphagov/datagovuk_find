1868c036430ed235ef67b0131512d1a493fad84b
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2024-12-20 17:24:41 +0000

    [WIP] Add previews

--- app/controllers/previews_controller.rb
+++ app/controllers/previews_controller.rb
@@ -1,10 +1,14 @@
 class PreviewsController < ApplicationController
   def show
     append_content_security_policy_directives(
-      connect_src: ["s3-eu-west-1.amazonaws.com", ENV["CKAN_DOMAIN"]].compact,
+      connect_src: ["s3-eu-west-1.amazonaws.com", "ckan.publishing.service.gov.uk"].compact,
     )
 
-    @dataset = Dataset.get_by_uuid(uuid: params[:dataset_uuid])
+    if ENV["USE_SOLR"]
+      @dataset = SolrDataset.get_by_uuid(uuid: params[:dataset_uuid])
+    else
+      @dataset = Dataset.get_by_uuid(uuid: params[:dataset_uuid])
+    end
     @datafile = @dataset.datafiles.find { |f| f.uuid == params[:datafile_uuid] }
 
     raise Datafile::DatafileNotFound if @datafile.nil?
