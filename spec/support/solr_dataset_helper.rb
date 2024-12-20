9835563ea9a28b67f394adab5009f11102cc38a0
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2024-12-20 17:21:19 +0000

    Add handling for when a dataset doesn't exist
    We want uses to see Not found page rather than Internal Server error.

--- spec/support/solr_dataset_helper.rb
+++ spec/support/solr_dataset_helper.rb
@@ -29,3 +29,14 @@ def modified_dataset_json(property, new_value)
 
   parsed_json
 end
+
+def mock_solr_http_error(status:, body: "Error response")
+  response_double = double("response")
+  allow(response_double).to receive(:[]).with(:status).and_return(status)
+  allow(response_double).to receive(:[]).with(:body).and_return(body)
+  allow(response_double).to receive(:[]).with(:headers).and_return("")
+  allow(response_double).to receive(:[]=).with(:status, status)
+  allow(response_double).to receive(:[]=).with(:body, body)
+
+  allow(Search::Solr).to receive(:search).and_raise(RSolr::Error::Http.new("Bad Request", response_double))
+end
