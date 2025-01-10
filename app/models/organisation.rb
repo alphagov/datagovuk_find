f45697f7fd1e4feac7f029c9f889529a3ac8aa35
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2025-01-10 11:01:44 +0000

    Remove Organisation hash keys used by OpenSearch
    We're no longer using OpenSeach. Solr uses different keys.

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -5,10 +5,10 @@ class Organisation
   def initialize(hash)
     @title = hash["title"]
     @name = hash["name"]
-    @contact_email = hash["contact_email"] || hash["extras_contact-email"]
-    @contact_name = hash["contact_name"] || hash["extras_contact-name"]
-    @foi_email = hash["foi_email"] || hash["extras_foi-email"]
-    @foi_web = hash["foi_web"] || hash["extras_foi-web"]
-    @foi_name = hash["foi_name"] || hash["extras_foi-name"]
+    @contact_email = hash["extras_contact-email"]
+    @contact_name = hash["extras_contact-name"]
+    @foi_email = hash["extras_foi-email"]
+    @foi_web = hash["extras_foi-web"]
+    @foi_name = hash["extras_foi-name"]
   end
 end
