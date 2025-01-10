97d5323f92c5c5cfdfea82e41752b74d2bff9d60
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2025-01-10 13:44:54 +0000

    Handle Solr connection errors
    Minor change to the users. This will avoid ActionView::Template::Error when we can't connect to Solr.
    
    https://govuk.sentry.io/issues/6204257119/?project=1461892
    
    We want to log it into Sentry for a few months so check if this is a bigger availability issue we need to address.
    
    Error class: https://github.com/rsolr/rsolr/blob/a60ec42b58f3b068f23537e49d0b6510bb12ee17/lib/rsolr/error.rb#L68
    
    Template copied from GOV.UK: https://github.com/alphagov/frontend/blob/main/app/views/static_error_pages/503.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,12 @@ class ApplicationController < ActionController::Base
   rescue_from SolrDataset::NotFound, with: :render_not_found
   rescue_from SolrDatafile::NotFound, with: :render_not_found
 
+  rescue_from RSolr::Error::ConnectionRefused do |exception|
+    Sentry.capture_exception(exception) if defined?(Sentry)
+
+    render "errors/service_unavailable_error", status: :service_unavailable
+  end
+
   def render_not_found
     render "errors/not_found", status: :not_found
   end
