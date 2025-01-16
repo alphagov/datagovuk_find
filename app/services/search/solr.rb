225142e7452cf0e83dbf396101d4f7a2ee7f071b
kentsanggds <ken.tsang@digital.cabinet-office.gov.uk>
2025-01-16 16:23:12 +0000

    d add puts to filter query

--- app/services/search/solr.rb
+++ app/services/search/solr.rb
@@ -33,6 +33,8 @@ module Search
       format_param = params.dig(:filters, :format)
       licence_param = params.dig(:filters, :licence_code)
 
+      puts "Building filter query"
+
       filter_query = ["state:active type:dataset"]
       filter_query << publisher_filter(publisher_param) if publisher_param.present?
       filter_query << topic_filter(topic_param) if topic_param.present?
