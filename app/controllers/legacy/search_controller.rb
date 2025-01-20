a08aafc6185f3502b8b1feb43ff93193799aa8f3
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2025-01-20 11:16:11 +0000

    Use strong parameters in Legacy::SearchController
    We want to explicitly allow selected parameters. Any other parameter will be
    ignored, preventing the `ActionController::UnfilteredParameters` error.
    
    https://govuk.sentry.io/issues/6229673891?project=1461892
    
    ActionController::UnfilteredParameters
    unable to convert unpermitted parameters to hash (ActionController::UnfilteredParameters)
    
            raise UnfilteredParameters
                  ^^^^^^^^^^^^^^^^^^^^

--- app/controllers/legacy/search_controller.rb
+++ app/controllers/legacy/search_controller.rb
@@ -1,17 +1,29 @@
 class Legacy::SearchController < ApplicationController
   def redirect
     filters = {
-      format: params["res_format"],
-      publisher: params["publisher"],
+      format: search_params[:res_format],
+      publisher: search_params[:publisher],
       licence: licence_param,
     }.compact
 
-    redirect_to search_path(q: params["q"], filters:)
+    redirect_to search_path(q: search_params[:q], filters:)
   end
 
 private
 
   def licence_param
-    "uk-ogl" if params["license_id-is-ogl"] == "true"
+    "uk-ogl" if search_params["license_id-is-ogl"] == "true"
+  end
+
+  def search_params
+    params.permit(
+      :q,
+      # Legacy filter params
+      :res_format,
+      :publisher,
+      "license_id-is-ogl",
+      # Current filter params
+      filters: %i[publisher topic format licence_code],
+    )
   end
 end
