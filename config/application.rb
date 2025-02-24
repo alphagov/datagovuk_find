d215a52373895db30966da3b71ef0ab63ec9edae
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2025-02-24 17:37:16 +0000

    [WIP] Run `rails app:update`
    - added load File.expand_path("spring", __dir__) in bin/rails, bin/rake
    
    In config/environments/production.rb:
    - added config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present? back (the default is config.public_file_server.enabled = false)
    - added config.assets.js_compressor = :uglifier back
    - added config.sass.style = :compressed back
    - added config.sass.line_comments = false back
    
    In config/environments/test.rb:
    - added config.assets.css_compressor = nil back
    - added config.eager_load = ENV["CI"].present? back (default = false)
    
    In config/initializers/assets.rb:
    - added Rails.application.config.assets.prefix = "/find-assets" back
    - added Rails.application.config.assets.paths << Rails.root.join("node_modules") back
    
    In config/application.rb:
    - added require "sprockets/railtie" back
    - added config.action_dispatch.default_headers back
    - added config.assets.css_compressor = nil back
    - added config.ssl_options = { hsts: { expires: 1.week } } back
    - added config.action_dispatch.default_headers back
    - config.i18n.load_path  can be removed because that's the default

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module FindDataBeta
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     config.ssl_options = { hsts: { expires: 1.week } }
 
