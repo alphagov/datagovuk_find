e1c989ef9a886be738e3e7d7c5770db5b6a84c37
Obsiye <abdirizak.obsiye@digital.cabinet-office.gov.uk>
2026-01-19 09:09:28 +0000

    DGUK-115 Guard against empty string in env
    Update the rack_attack.rb config
    If the env value is an empty string then use the default value 120 for RATE_LIMIT_COUNT and 60 for RATE_LIMIT_PERIOD
    
    Test that rack-attack doesn't cause a 500 http response if the rate limit envs are ""

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,7 +1,7 @@
 class Rack::Attack
   Rack::Attack.enabled = !Rails.env.test?
-  RATE_LIMIT_COUNT  = ENV.fetch("RATE_LIMIT_COUNT", 120).to_i
-  RATE_LIMIT_PERIOD = ENV.fetch("RATE_LIMIT_PERIOD", 60).to_i
+  RATE_LIMIT_COUNT = (ENV.fetch("RATE_LIMIT_COUNT", nil).presence || 120).to_i
+  RATE_LIMIT_PERIOD = (ENV.fetch("RATE_LIMIT_PERIOD", nil).presence || 60).to_i
 
   throttle("/search", limit: RATE_LIMIT_COUNT, period: RATE_LIMIT_PERIOD) do |request|
     if request.path == "/search"
