e479139f0db751716492bafce7b724d15b0f1461
Obsiye <abdirizak.obsiye@digital.cabinet-office.gov.uk>
2026-01-20 15:26:39 +0000

    DGUK-137 Update content of notification banner

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -18,10 +18,10 @@ RSpec.feature "Home page", type: :feature do
 
   def and_i_can_see_a_notification_banner
     expect(page).to have_content(I18n.t(".pages.home.notification_banner_title"))
-    expect(page).to have_css(".govuk-notification-banner", text: "We are planning improvements to this service. Help us learn what works and what does not by taking part in this data.gov.uk survey")
+    expect(page).to have_css(".govuk-notification-banner", text: "We're planning improvements to this service. Help us learn what to change by completing a short survey.")
   end
 
   def and_the_notification_banner_has_a_link
-    expect(page).to have_link("data.gov.uk survey", href: "https://surveys.publishing.service.gov.uk/s/OSC03D/", count: 1)
+    expect(page).to have_link("completing a short survey", href: "https://surveys.publishing.service.gov.uk/s/OSC03D/", count: 1)
   end
 end
