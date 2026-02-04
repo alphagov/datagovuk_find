e77157f971ee8b4f78e884e08518f1e613dfd4b7
Obsiye <abdirizak.obsiye@digital.cabinet-office.gov.uk>
2026-02-04 11:32:44 +0000

    DGUK-181 DRY collection service

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -2,19 +2,19 @@ class CollectionsController < ApplicationController
   def show
     @collections_service = CollectionsService.new(params[:collection], params[:topic])
 
-    unless @collections_service.collection_topic?(params[:collection], params[:topic]) && FeatureFlag.enabled?(:version_2_collections)
+    unless @collections_service.valid_collection_topic? && FeatureFlag.enabled?(:version_2_collections)
       render_not_found && return
     end
 
     render collection_topic_path, layout: "version_2/collection", locals: {
       collections: @collections_service.collections_slugs,
-      collection: @collections_service.collection_name,
+      collection: @collections_service.collection,
       side_navigations: @collections_service.side_navigations,
       current_topic: @collections_service.topic,
     }
   end
 
   def collection_topic_path
-    "generated/collections/#{@collections_service.collection_name}/#{@collections_service.topic}"
+    "generated/collections/#{@collections_service.collection}/#{@collections_service.topic}"
   end
 end
--- app/services/collections_service.rb
+++ app/services/collections_service.rb
@@ -1,19 +1,52 @@
 class CollectionsService
-  attr_reader :collection_name, :topic_name
+  attr_reader :collection, :topic_name
 
   COLLECTIONS_LOCATION = Rails.configuration.x.generated_collections_location
 
-  def initialize(collection_name, topic_name = nil)
-    @collection_name = collection_name
+  def initialize(collection, topic_name = nil)
+    @collection = collection
     @topic_name = topic_name
   end
 
-  def collection_topic?(collection, topic = nil)
+  def valid_collection_topic?
     collection?(collection) && topic?(topic)
   end
 
   def topic
     @topic_name.presence || first_topic
+  end
+
+  def side_navigations
+    @side_navigations ||= topics.map do |side_navigation|
+      side_navigation.gsub(".html.erb", "")
+    end
+  end
+
+  def collections_slugs
+    Dir.entries(Rails.root.join(COLLECTIONS_LOCATION)).sort
+    .reject { |files|
+      [".", ".."].include?(files)
+    }.map do |collection|
+      OpenStruct.new(
+        slug: collection,
+        title: collection.gsub("-", " ").humanize,
+      )
+    end
+  end
+
+private
+
+  def topics
+    Dir.entries(Rails.root.join(COLLECTIONS_LOCATION, @collection)).sort
+    .reject do |entry|
+      [".", ".."].include?(entry)
+    end
+  end
+
+  def topic?(topic = nil)
+    return true if topic.blank?
+
+    Rails.root.join(COLLECTIONS_LOCATION, @collection, "#{topic}.html.erb").exist?
   end
 
   def first_topic
@@ -22,36 +55,5 @@ class CollectionsService
 
   def collection?(collection)
     Rails.root.join(COLLECTIONS_LOCATION, collection).exist?
-  end
-
-  def topic?(topic)
-    return true if topic.blank?
-
-    Rails.root.join(COLLECTIONS_LOCATION, @collection_name, "#{topic}.html.erb").exist?
-  end
-
-  def side_navigations
-    @side_navigations ||= topics.map do |entry|
-      entry.gsub(".html.erb", "")
-    end
-  end
-
-  def topics
-    Dir.entries(Rails.root.join(COLLECTIONS_LOCATION, @collection_name)).sort
-    .reject do |entry|
-      [".", ".."].include?(entry)
-    end
-  end
-
-  def collections_slugs
-    Dir.entries(Rails.root.join("app/views/generated/collections")).sort
-    .reject { |entry|
-      [".", ".."].include?(entry)
-    }.map do |entry|
-      OpenStruct.new(
-        slug: entry,
-        title: entry.gsub("-", " ").humanize,
-      )
-    end
   end
 end
