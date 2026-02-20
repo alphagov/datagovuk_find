4c02a6e96e568cfbe7734c6f8889672cc12a7e5a
Joe Lanman <joelanman@gmail.com>
2026-02-20 22:11:54 +0000

    new header

--- app/controllers/v2/pages_controller.rb
+++ app/controllers/v2/pages_controller.rb
@@ -2,12 +2,10 @@ module V2
   class PagesController < ApplicationController
     def home
       expires_in 30.minutes, public: true
-      @collections = []
       render layout: "v2/layouts/application"
     end
 
     def components
-      @collections = []
       render layout: "v2/layouts/application"
     end
   end
