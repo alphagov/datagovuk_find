2507a05f079259de0245fdb7c4824885d4f637b4
Brendan Smith <brendan.smith@digital.cabinet-office.gov.uk>
2026-02-25 16:54:38 +0000

    Add stub data manual content page

--- app/controllers/v2/data_manual_controller.rb
+++ app/controllers/v2/data_manual_controller.rb
@@ -2,7 +2,26 @@ module V2
   class DataManualController < ApplicationController
     def home
       expires_in 30.minutes, public: true
-      render layout: "v2/layouts/application"
+      render layout: "v2/layouts/data_manual"
+    end
+
+    def content
+      expires_in 30.minutes, public: true
+      @collections = []
+      data_manual_content_slugs = [
+        "who-this-manual-is-for",
+        "data-management",
+        "data-standards",
+        "security",
+        "data-protection-and-privacy",
+        "data-sharing",
+        "ai-and-data-driven-technologies",
+        "general-guidance",
+      ]
+      unless data_manual_content_slugs.include? params[:content_slug]
+        render_not_found && return
+      end
+      render layout: "v2/layouts/data_manual"
     end
   end
 end
