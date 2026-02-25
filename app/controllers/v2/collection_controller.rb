8b4961e0ddf985e4636d16099d2f6f8bb47d71d9
Obsiye <abdirizak.obsiye@digital.cabinet-office.gov.uk>
2026-02-25 15:14:12 +0000

    DGUK-148 Refactor collection controller
    Move view_template logic into the collection service to meet the single responsibility principle

--- app/controllers/v2/collection_controller.rb
+++ app/controllers/v2/collection_controller.rb
@@ -9,16 +9,12 @@ module V2
       @collection = collections_service.collection
       @side_navigations = collections_service.side_navigations
       @current_topic = collections_service.topic
-      @view_template = collection_topic_path
+      @view_template = collections_service.view_template_path
 
       render template: "v2/collection/collection", layout: "v2/layouts/application"
     end
 
   private
-
-    def collection_topic_path
-      "generated/collections/#{collections_service.collection}/#{collections_service.topic}"
-    end
 
     def collections_service
       @collections_service ||= CollectionsService.new(params[:collection], params[:topic])
