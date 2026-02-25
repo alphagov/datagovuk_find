426431e3526d5b51e78ad6e5e12a7e16bc0402ac
Obsiye <abdirizak.obsiye@digital.cabinet-office.gov.uk>
2026-02-25 15:51:48 +0000

    DGUK-148 fix broken tests after removing side_nav feature

--- spec/features/collections_spec.rb
+++ spec/features/collections_spec.rb
@@ -5,13 +5,6 @@ RSpec.feature "collections", type: :feature do
     given_i_am_on_the_collection_page
     and_top_navigation_has_drop_down_of_collections
     then_i_can_see_the_collection_title("Land and property")
-    and_i_can_see_the_collection_navigation_links
-    and_the_topic_is_highlighted_in_navigation("Property price paid")
-    then_i_can_see_the_topic_content("Property price paid")
-    when_i_click_on_a_different_topic("Planning data")
-    then_i_can_see_the_collection_title("Land and property")
-    and_the_topic_is_highlighted_in_navigation("Planning data")
-    then_i_can_see_the_topic_content("Planning data")
   end
 
   def given_i_am_on_the_collection_page
@@ -20,11 +13,6 @@ RSpec.feature "collections", type: :feature do
 
   def then_i_can_see_the_collection_title(title)
     expect(page).to have_content(title)
-  end
-
-  def and_i_can_see_the_collection_navigation_links
-    expect(page).to have_link("Planning data", href: "/collections/land-and-property/planning-data")
-    expect(page).to have_link("Property price paid", href: "/collections/land-and-property/property-price-paid")
   end
 
   def then_i_can_see_the_topic_content(topic_name)
@@ -36,25 +24,14 @@ RSpec.feature "collections", type: :feature do
     end
   end
 
-  def and_the_topic_is_highlighted_in_navigation(topic_name)
-    within(".datagovuk-side-navigation") do
-      expect(page).to have_css(".datagovuk-side-navigation__item--active", text: topic_name)
-    end
-  end
-
-  def when_i_click_on_a_different_topic(topic_name)
-    click_link topic_name
-  end
-
   def and_top_navigation_has_drop_down_of_collections
-    within(".datagovuk-nav-dropdown__menu") do
+    within(".dgu-nav-dropdown__menu") do
       links = [
         "Business and economy",
         "Environment",
         "Government",
         "Land and property",
         "People",
-        "Population",
         "Transport",
       ]
       links.each do |link_text|
