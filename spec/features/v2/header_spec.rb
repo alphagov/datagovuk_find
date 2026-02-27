ce0621a70f17970b48a536c0ea88ac9b37fa8ddc
Obsiye <abdirizak.obsiye@digital.cabinet-office.gov.uk>
2026-02-27 15:57:51 +0000

    Refactor feature test

--- spec/features/v2/header_spec.rb
+++ spec/features/v2/header_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe "header", type: :feature do
     given_i_am_on_the_home_page
     then_i_can_see_the_header
     and_top_navigation_has_drop_down_of_collections
-    and_i_click_on_collections_menu
+    and_i_click_on_collections_menu_button
     then_i_can_see_the_collections_menu
-    and_i_click_on_data_manual_menu
+    and_i_click_on_data_manual_menu_button
     then_i_can_see_the_data_manual_menu
   end
 
@@ -36,22 +36,22 @@ RSpec.describe "header", type: :feature do
   end
 
   def then_i_can_see_the_collections_menu
-    expect(page).to have_css("#datagovuk-menu-collections", visible: true)
+    expect(page).to have_css("#datagovuk-header-button-collections", visible: true)
   end
 
-  def and_i_click_on_collections_menu
+  def and_i_click_on_collections_menu_button
     find("#datagovuk-header-button-collections").click
   end
 
   def then_i_can_see_the_collections_menu
-    expect(page).to have_css("#datagovuk-menu-collections", visible: true)
+    expect(page).to have_css("#datagovuk-header-button-collections", visible: true)
   end
 
-  def and_i_click_on_data_manual_menu
+  def and_i_click_on_data_manual_menu_button
     find("#datagovuk-header-button-data-manual").click
   end
 
   def then_i_can_see_the_data_manual_menu
-    expect(page).to have_css("#datagovuk-menu-data-manual", visible: true)
+    expect(page).to have_css("#datagovuk-header-button-data-manual", visible: true)
   end
 end
--- spec/features/v2/mobile/header_spec.rb
+++ spec/features/v2/mobile/header_spec.rb
@@ -1,58 +1,47 @@
 require "rails_helper"
 
-RSpec.describe "header", type: :feature do
-  scenario "viewing the header on mobile", js: true do
-    given_i_am_on_the_home_page
-    and_im_on_mobile_view
-    then_i_can_see_a_menu_button
-    and_i_do_not_see_the_collections_menu
-    and_i_do_not_see_the_data_manual_menu
-    then_i_click_on_the_menu_button
-    and_i_can_see_the_collections_menu
-    and_i_can_see_the_data_manual_menu
-    and_i_click_on_collections_menu
-    then_i_do_not_see_the_data_manual_menu
-    and_i_do_not_see_the_collections_menu
+RSpec.describe "Header navigation", type: :feature do
+  scenario "Toggling the mobile menu", js: true do
+    given_i_am_on_the_home_page_on_mobile
+
+    then_the_mobile_menu_is_closed
+    and_the_menu_is_not_visible
+
+    when_i_click_the_menu_button
+    then_the_mobile_menu_is_open
+    and_the_menu_is_visible
+
+    when_i_click_the_menu_button
+    then_the_mobile_menu_is_closed
+    and_the_menu_is_not_visible
   end
 
-  def given_i_am_on_the_home_page
+private
+
+  def given_i_am_on_the_home_page_on_mobile
+    page.driver.browser.manage.window.resize_to(375, 812)
     visit root_path
   end
 
-  def and_im_on_mobile_view
-    page.driver.browser.manage.window.resize_to(375, 812)
-    expect(page).to have_css(".datagovuk-header__button", visible: true)
+  def when_i_click_the_menu_button
+    find("button", text: "Menu").click
   end
 
-  def then_i_can_see_a_menu_button
-    expect(page).to have_css("#datagovuk-header-button-all", visible: true)
+  def then_the_mobile_menu_is_closed
+    expect(find("button", text: "Menu")["aria-expanded"]).to eq("false")
   end
 
-  def and_i_do_not_see_the_collections_menu
-    expect(page).to have_css("#datagovuk-menu-collections", visible: false)
+  def and_the_menu_is_not_visible
+    expect(page).not_to have_css("#datagovuk-menu-collections", visible: true)
+    expect(page).not_to have_css("#datagovuk-menu-data-manual", visible: true)
   end
 
-  def and_i_do_not_see_the_data_manual_menu
-    expect(page).to have_css("#datagovuk-menu-data-manual", visible: false)
-  end
-
-  def then_i_click_on_the_menu_button
-    find("#datagovuk-header-button-all").click
-  end
-
-  def and_i_can_see_the_collections_menu
+  def and_the_menu_is_visible
     expect(page).to have_css("#datagovuk-menu-collections", visible: true)
-  end
-
-  def and_i_click_on_collections_menu
-    find("#datagovuk-menu-collections").click
-  end
-
-  def and_i_can_see_the_data_manual_menu
     expect(page).to have_css("#datagovuk-menu-data-manual", visible: true)
   end
 
-  def then_i_do_not_see_the_data_manual_menu
-    expect(page).to have_css("#datagovuk-menu-data-manual", visible: false)
+  def then_the_mobile_menu_is_open
+    expect(find("#datagovuk-header-button-all")["aria-expanded"]).to eq("true")
   end
 end
