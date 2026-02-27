5007e8a930a95853109334f7e2352a0bfee66b8a
Brendan Smith <brendan.smith@digital.cabinet-office.gov.uk>
2026-02-27 16:23:29 +0000

    Combine feedback components

--- app/controllers/v2/data_manual_controller.rb
+++ app/controllers/v2/data_manual_controller.rb
@@ -2,7 +2,6 @@ module V2
   class DataManualController < ApplicationController
     def home
       expires_in 30.minutes, public: true
-      @collections = []
       render layout: "v2/layouts/application"
     end
   end
