0903dcc7ec707b2f6f3ae28d63d2c2c585353657
Brendan Smith <brendan.smith@digital.cabinet-office.gov.uk>
2026-03-02 13:59:09 +0000

    Fix brakeman complaints

--- app/controllers/v2/data_manual_controller.rb
+++ app/controllers/v2/data_manual_controller.rb
@@ -10,16 +10,18 @@ module V2
     def content
       expires_in 30.minutes, public: true
       data_manual_markdown_directory = Rails.configuration.x.markdown_data_manual_location
-      markdown_file = Rails.root.join(data_manual_markdown_directory, "#{params[:slug]}.md")
+      safe_slug = params[:slug].parameterize
+      markdown_file = Rails.root.join(data_manual_markdown_directory, "#{safe_slug}.md")
       if File.exist?(markdown_file)
         markdown = File.read(markdown_file)
       else
         render_not_found && return
       end
       rendered_content = Dgu::Markdown.render(markdown).html_safe
+
       data_manual_pages = @data_manual_pages.deep_dup
       data_manual_pages.each do |data_manual_item|
-        if data_manual_item[:slug] == params[:slug]
+        if data_manual_item[:slug] == safe_slug
           data_manual_item[:selected] = true
         end
       end
