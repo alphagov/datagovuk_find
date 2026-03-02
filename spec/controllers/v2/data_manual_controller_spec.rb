32cf72a6cf29d2a12de0665096c0ecf0bf201d5a
Brendan Smith <brendan.smith@digital.cabinet-office.gov.uk>
2026-03-02 16:26:40 +0000

    Add tests for DataManualController content method

---
+++ spec/controllers/v2/data_manual_controller_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe "Posts", type: :request do
+
+  describe "GET /data-manual/:slug" do
+    
+    context "when the markdown content exists" do
+      it "returns a 200 status code" do
+        get "/data-manual/who-this-manual-is-for"
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "when the markdown content does not exist" do
+      it "returns a 404 status code" do
+        get "/data-manual/non-existent-slug"
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+
+  end
+end
