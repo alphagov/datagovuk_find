1323fa6b25f6e336cd209faced92a2f5197dde90
Brendan Smith <brendan.smith@digital.cabinet-office.gov.uk>
2026-03-04 10:58:26 +0000

    Make collection root controller redirect to first topic in collection

--- app/controllers/v2/collection_controller.rb
+++ app/controllers/v2/collection_controller.rb
@@ -1,6 +1,6 @@
 module V2
   class CollectionController < ApplicationController
-    def show
+    def collection_topic
       unless collections_service.valid_collection_topic?
         render_not_found && return
       end
@@ -12,6 +12,13 @@ module V2
       @view_template = collections_service.view_template_path
 
       render template: "v2/collection/collection", layout: "v2/layouts/application"
+    end
+
+    def collection
+      unless collections_service.valid_collection_topic?
+        render_not_found && return
+      end
+      redirect_to collection_topic_path(collection: params[:collection], topic: collections_service.first_topic), status: :found and return
     end
 
   private
