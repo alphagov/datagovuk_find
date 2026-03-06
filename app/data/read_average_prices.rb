817ab1936ac7a1fe879c3c1afd22d68c71c2a4a6
Obsiye <abdirizak.obsiye@digital.cabinet-office.gov.uk>
2026-03-06 08:03:04 +0000

    Line chart looking good

--- app/data/read_average_prices.rb
+++ app/data/read_average_prices.rb
@@ -1,28 +1,30 @@
-require 'csv'
-require 'json'
-require 'date'
+require "csv"
+require "json"
+require "date"
 
 def convert_csv_to_regional_json(input_file, output_file)
-  grouped_data = Hash.new { |hash, key| hash[key] = [] }
+  temp_grouped = Hash.new { |hash, key| hash[key] = {} }
 
-  puts(input_file)
   CSV.foreach(input_file, headers: true) do |row|
-    date_obj = Date.parse(row['Date'])
-    formatted_date = date_obj.strftime('%Y-%m-%d')
+    date_obj = Date.parse(row["Date"])
+    formatted_date = date_obj.strftime("%Y-%m-%d")
 
-    record = [
-      formatted_date,
-      row['Average_Price'].to_f
-    ]
-
-    grouped_data[row['Region_Name']] << record
+    temp_grouped[row["Region_Name"]][formatted_date] = row["Average_Price"].to_f
   end
 
-  File.open(output_file, 'w') do |f|
-    f.write(JSON.pretty_generate(grouped_data))
+  final_data = temp_grouped.map do |region, dates|
+    {
+      name: region,
+      size: dates.values.compact.size,
+      maxValueRoundedUp: (dates.values.compact.max.ceil / 10_000.0).ceil * 10_000,
+      minValueRoundedDown: (dates.values.compact.min.floor / 10_000.0).floor * 10_000,
+      data: dates.transform_values { |price| price / 1000 },
+    }
   end
 
-  puts "Successfully converted #{input_file} to #{output_file}"
+  File.open(output_file, "w") do |f|
+    f.write(JSON.pretty_generate(final_data))
+  end
 end
 
-convert_csv_to_regional_json('/Users/ObsiyeA/Documents/ndl/datagovuk_find/app/data/Average-prices-68to25.csv', 'regional_prices.json')
+convert_csv_to_regional_json("/Users/ObsiyeA/Documents/ndl/datagovuk_find/app/data/Average-prices-68to25.csv", "regional_prices.json")
