c0317d3786dddd47b54ef34fb5c44c0951ff7024
Obsiye <abdirizak.obsiye@digital.cabinet-office.gov.uk>
2026-03-18 08:49:36 +0000

    DGUK-293 fix test
    Move the Rails.configuration out of a constant so that it's easier to test e.g. update the location
    using allow(Rails.configuration..x.visualisations_data_location = new test location)
    
    This should fix the charts test from failing in CI/CD

--- app/controllers/v2/charts_controller.rb
+++ app/controllers/v2/charts_controller.rb
@@ -1,7 +1,6 @@
 module V2
   class ChartsController < ApplicationController
     ALLOWED_EXTENSIONS = %w[.csv .xlsx .xls].freeze
-    BASE_DIR = Rails.root.join(Rails.configuration.x.visualisations_data_location).realpath
 
     def download
       if chart_files.any?
@@ -15,6 +14,10 @@ module V2
 
   private
 
+    def base_dir
+      Rails.root.join(Rails.configuration.x.visualisations_data_location)
+    end
+
     def safe_topic
       File.basename(params[:topic])
     end
@@ -24,7 +27,7 @@ module V2
     end
 
     def chart_files
-      pattern = BASE_DIR.join(safe_topic, "#{safe_chart}.*").to_s
+      pattern = base_dir.join(safe_topic, "#{safe_chart}.*").to_s
 
       Dir.glob(pattern).select do |path|
         ALLOWED_EXTENSIONS.include?(File.extname(path).downcase)
