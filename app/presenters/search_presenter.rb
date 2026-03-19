07591613bf2b3bcdded2ea2e842753c0901b3c55
Obsiye <abdirizak.obsiye@digital.cabinet-office.gov.uk>
2026-03-19 14:45:24 +0000

    Show remove filters when filters are applied

--- app/presenters/search_presenter.rb
+++ app/presenters/search_presenter.rb
@@ -65,6 +65,10 @@ class SearchPresenter
     search_params.fetch("q", "")
   end
 
+  def filters_applied?
+    [search_keywords, selected_publisher, selected_topic, selected_format, selected_licence].any?(&:present?)
+  end
+
   def selected_publisher
     search_params.dig(:filters, :publisher)
   end
