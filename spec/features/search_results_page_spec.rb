a7827e40a4398f23175723e0d0260b13afe8987f
Joe Lanman <joelanman@gmail.com>
2026-03-22 18:41:21 +0000

    update directory banner

--- spec/features/search_results_page_spec.rb
+++ spec/features/search_results_page_spec.rb
@@ -22,6 +22,6 @@ RSpec.feature "Search directory page", type: :feature do
   end
 
   def and_i_can_see_a_notification_banner
-    expect(page).to have_content("data.gov.uk is changing and you may experience some issues with the data in this directory.")
+    expect(page).to have_content("We’re making changes to data.gov.uk")
   end
 end
