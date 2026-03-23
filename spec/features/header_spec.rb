361dfa797882a6787fcb3e4ce2cc07d74658b30a
Obsiye <abdirizak.obsiye@digital.cabinet-office.gov.uk>
2026-03-23 10:34:21 +0000

    Refactor and remove unrequired code

--- spec/features/header_spec.rb
+++ spec/features/header_spec.rb
@@ -4,16 +4,13 @@ RSpec.feature "directory header", type: :feature do
   scenario "displays the directory header" do
     given_i_am_on_the_search_directory_page
     and_i_can_see_a_link_to_the_search_directory
-    then_i_can_se_the_directory_link
+    then_i_can_see_the_directory_link
     and_i_can_see_a_link_to_the_search_directory_in_the_mobile_menu
   end
 
   def given_i_am_on_the_search_directory_page
-    allow(Search::Solr).to receive(:search).and_return(JSON.parse(File.read(Rails.root.join("spec/fixtures/solr_response.json").to_s)))
     allow(Search::Solr).to receive(:get_organisations).and_return({
       "Aberdeen City Council" => "aberdeen-city-council",
-      "Ministry of Housing, Communities and Local Government" => "department-for-communities-and-local-government",
-      "Academics" => "academics",
     })
     visit search_path
   end
@@ -33,7 +30,7 @@ RSpec.feature "directory header", type: :feature do
     end
   end
 
-  def then_i_can_se_the_directory_link
+  def then_i_can_see_the_directory_link
     within(".datagovuk-header") do
       expect(page).to have_link("Directory", href: search_path, class: "datagovuk-header__link")
     end
